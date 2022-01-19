dc390a9b0f443b9c96f8cabc68709c3e30f6e8b9
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-01-19 13:10:17 +0100

    Add internal endpoint

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "tempo_endpoint" {
   value       = cloudfoundry_route.tempo.endpoint
 }
 
+output "tempo_internal_endpoint" {
+  description = "The internal endpoint where Tempo is reachable on"
+  value       = cloudfoundry_route.tempo_internal.endpoint
+}
+
 output "tempo_id" {
   description = "The Tempo apps' id"
   value       = cloudfoundry_app.tempo.id
